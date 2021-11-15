46d6227cb15b1ca1855627a6df296c07b815aab3
Mujahid6890 <mujahidsaleem1002@gmail.com>
2021-11-15 22:26:26 +0500

    Front page

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView git;
+    private Button quiz;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        quiz = (Button) findViewById(R.id.quiz);
+        quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+        git = findViewById(R.id.git);
+        git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://github.com/Mujahid6890/app3.git");
+            }
+        });
+
+    }
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
+
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, Quiz.class);
+        startActivity(intent);
+    }
+
+}
